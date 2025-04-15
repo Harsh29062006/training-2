42b7edbc47f634007dfe46d5c149a16b522d99d5
Harsh yadav <harsh_249023@saitm.ac.in>
2025-04-15 10:13:20 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,23 @@
+console.log("---welcome in js----")
+
+//const myHTML = `<h1>added from JS</h1>
+//<h1> harsh </h1>`;
+//const htmlEle = document.getElementById("container");
+//const htmlEle2 = document.getElementsByClassName("container2")[0];
+//htmlEle.innerHTML = myHTML;
+//htmlEle2.innerHTML = myHTML;
+
+console.log("----functions----")
+function sum(a, b){
+    const result =a +b;
+    console.log("sum :", result);
+    HTMLElement.innerHTML = result;
+    return result;
+
+}
+function sub(a, b){
+    const result= a-b;
+    return result;
+} 
+const ans = sub(6, 7);
+console.log(ans);
